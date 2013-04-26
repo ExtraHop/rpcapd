3eea31302873e5e71cdffd926612a2622def242f
alex <alex@3294381c-b82a-0410-885b-c1e364fe48a6>
2013-04-26 06:33:04 +0000

    rpcapd: Update installer and sysv init script.
    The install can now install from files on the local machine, in addition
    to fetching them from a uri.

--- winpcap/wpcap/libpcap/rpcapd/rpcapd-dist/install-rpcapd.sh
+++ winpcap/wpcap/libpcap/rpcapd/rpcapd-dist/install-rpcapd.sh
@@ -1,71 +1,167 @@
 #!/bin/sh
 
-# curl -k 'https://<extrahop_ip>/rpcapd/install-rpcapd.sh' > install-rpcapd.sh && sudo sh ./install-rpcapd.sh <extrahop_ip> <rpcap_port_from_running_config>
+# curl --fail -k 'https://<extrahop_ip>/rpcapd/install-rpcapd.sh' > install-rpcapd.sh && sudo sh ./install-rpcapd.sh <extrahop_ip> <rpcap_port_from_running_config>
 
-if [ "$#" -lt 2 ]; then
-    echo "Usage: $0 <extrahop ip> <rpcap port>" >&2
-    exit 2
-fi
-
-EH_IP="$1"
-RPCAP_PORT="$2"
-
-DRYRUN=
-RPCAPD_URI="https://$EH_IP/rpcapd"
 RPCAPD_BIN_PATH="/usr/sbin/rpcapd"
 RPCAPD_INIT_PATH="/etc/init.d/rpcapd"
 RPCAPD_CFG_PATH="/etc/rpcapd.ini"
-RPCAPD_BIN="rpcapd-64bit-linux"
-if [ ! $(getconf LONG_BIT) = "64" ]; then
-    RPCAPD_BIN="rpcapd-32bit-linux"
-fi
 
-fetch() {
-    URI="$RPCAPD_URI/$1"
-    echo "Fetching $2 from $URI"
-    OUT="$2"
-    if [ -n "$DRYRUN" ]; then
-        OUT="/dev/null"
-    fi
-    curl -k "$RPCAPD_URI/$1" > "$OUT"
+usage() {
+    cat 2<&1 <<EOM
+Usage: $0 <extrahop_ip> <rpcap_port>
+
+To install from files in the current directory:
+
+    $0 -i ./ <extrahop_ip> <rpcap_port>
+
+To only fetch files into the current directory but not install them:
+  (The script can be run again with -i ./ to actually install)
+
+    $0 -o ./ <extrahop_ip> <rpcap_port>
+
+
+Options:
+  -u 'https://foobar/rpcapd/': fetch files from https://foobar/rpcapd/ instead
+                               of https://<extrahop_ip>/rpcapd/.
+  -i <directory>: Use install files from directory, instead of fetching
+                  from <extrahop_ip>.
+  -o <directory>: Only output fetched files to <directory>, and don't install.
+                  After running -o <directory>, run with -i <directory> to
+                  actually install rpcapd.
+  -c: Only update the configuration file, $RPCAPD_CFG_PATH,
+      with <extrahop_ip> and <rpcap_port>
+  -h: Print help.
+EOM
+    exit 2
 }
 
-# stop rpcapd if there's already one on the system
-if [ -x "$RPCAPD_INIT_PATH" ]; then
+# ============================ Parse Arguments ================================
+
+fetch_uri=
+fetch_dir=
+no_fetch=
+fetch_only=
+cfg_update_only=
+
+# process arguments
+OPTIND=1
+while getopts "u:i:o:ch" opt; do
+    case "$opt" in
+        u) fetch_uri="$OPTARG";;
+        i) fetch_dir="$OPTARG"; no_fetch="y";;
+        o) fetch_dir="$OPTARG"; fetch_only="y";;
+        c) cfg_update_only="y";;
+        h) usage;;
+    esac
+done
+shift $((OPTIND-1))
+
+# positional arguments: <extrahop_ip> <rpcap_port>
+[ "$#" -lt 2 ] && usage
+eh_ip="$1"
+rpcap_port="$2"
+shift 2
+
+# any following arguments get passed to rpcapd
+[ "$1" = "--" ] && shift
+rpcapd_extra_args="$@"
+
+# ========================= Detect Platform ===================================
+
+arch="$(uname -m)"
+rpcapd_bin="rpcapd-64bit-linux"
+if [ "$arch" = "x86_64" ]; then
+    : # default
+elif [ "$arch" = "i386" -o "$arch" = "i486" -o "$arch" = "i586" -o \
+       "$arch" = "i686" ]; then
+    rpcapd_bin="rpcapd-32bit-linux"
+else
+    echo "Warning: unrecognized platform $arch, defaulting to $rpcapd_bin"
+fi
+
+rpcapd_init="rpcapd.debianinit"
+init_add_cmd="update-rc.d rpcapd defaults"
+if command -v start-stop-daemon >/dev/null 2>&1; then
+    : # default
+elif command -v chkconfig >/dev/null 2>&1; then
+    rpcapd_init="rpcapd.sysvinit"
+    init_add_cmd="chkconfig --add rpcapd"
+else
+    echo "Warning: unrecognized init system,"
+    echo "         defaulting to $rpcapd_init, $init_add_cmd"
+fi
+
+# ========================= Fetch Binary / init script ========================
+
+fetch() {
+    uri="$fetch_uri/$1"
+    out="$2"
+    echo "Fetching $uri > $out"
+    if ! curl --fail -k "$uri" > "$out"; then
+        echo "Error: Fetching $uri > $out failed!"
+        exit 1
+    fi
+}
+
+ensure_exists() {
+    if [ ! -f "$1" ]; then
+        echo "Error: Missing file $1"
+        exit 1
+    fi
+}
+
+[ -z "$fetch_uri" ] && fetch_uri="https://$eh_ip/rpcapd"
+
+if [ -n "$fetch_only" ]; then
+    RPCAPD_BIN_PATH="$fetch_dir/$rpcapd_bin"
+    RPCAPD_INIT_PATH="$fetch_dir/$rpcapd_init"
+    RPCAPD_CFG_PATH="$fetch_dir/rpcapd.ini"
+elif [ -x "$RPCAPD_INIT_PATH" ]; then
     $RPCAPD_INIT_PATH stop
 fi
-
-# download the files
-fetch "$RPCAPD_BIN" "$RPCAPD_BIN_PATH"
-chmod u+x "$RPCAPD_BIN_PATH"
-
-if command -v start-stop-daemon >/dev/null 2>&1; then
-    fetch "rpcapd.debianinit" "$RPCAPD_INIT_PATH"
-else
-    fetch "rpcapd.sysvinit" "$RPCAPD_INIT_PATH"
+if [ -z "$cfg_update_only" ]; then
+    if [ -n "$no_fetch" ]; then
+        ensure_exists "$fetch_dir/$rpcapd_bin"
+        ensure_exists "$fetch_dir/$rpcapd_init"
+        set +e
+        cp -f "$fetch_dir/$rpcapd_bin" "$RPCAPD_BIN_PATH"
+        cp -f "$fetch_dir/$rpcapd_init" "$RPCAPD_INIT_PATH"
+        set -e
+    else
+        fetch "$rpcapd_bin" "$RPCAPD_BIN_PATH"
+        fetch "$rpcapd_init" "$RPCAPD_INIT_PATH"
+    fi
+    set +e
+    chmod u+x "$RPCAPD_BIN_PATH"
+    chmod u+x "$RPCAPD_INIT_PATH"
+    set -e
 fi
-chmod u+x "$RPCAPD_INIT_PATH"
-if command -v start-stop-daemon >/dev/null 2>&1; then
-    update-rc.d rpcapd defaults
-else
-    chkconfig --add rpcapd
+
+if [ -n "$rpcapd_extra_args" ]; then
+    # update DAEMON_ARGS="..." in the init file
+    echo "Adding extra DAEMON_ARGS=\"... $rpcapd_extra_args\""
+    set +e
+    sed -i "s|^\(DAEMON_ARGS=\".*\)\"$|\1 $rpcapd_extra_args\"|" \
+           "$RPCAPD_INIT_PATH"
+    set -e
 fi
 
-# write out a config file
-CFG=$(cat <<EOF
-ActiveClient = $EH_IP,$RPCAP_PORT
+cfg=$(cat <<EOF
+ActiveClient = $eh_ip,$rpcap_port
 NullAuthPermit = YES
 EOF
 )
 
-echo "Writing $RPCAPD_CFG_PATH with contents:"
-echo "$CFG"
+echo "Writing config to $RPCAPD_CFG_PATH with contents:"
+echo "$cfg"
 
-if [ -z "$DRYRUN" ]; then
-    echo "$CFG" > "$RPCAPD_CFG_PATH"
-    chmod 644 "$RPCAPD_CFG_PATH"
-fi
+set +e
+echo "$cfg" > "$RPCAPD_CFG_PATH"
+chmod 644 "$RPCAPD_CFG_PATH"
+set -e
 
-if [ -x "$RPCAPD_INIT_PATH" ]; then
+if [ -z "$fetch_only" -a -x "$RPCAPD_INIT_PATH" ]; then
+    $init_add_cmd
     $RPCAPD_INIT_PATH start
 fi
+
