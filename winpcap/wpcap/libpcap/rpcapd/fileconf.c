3c1b5515c72cf8370b73d7df6cd49fb0208291bd
alex <alex@3294381c-b82a-0410-885b-c1e364fe48a6>
2013-08-30 02:00:08 +0000

    rpcapd: Allow ifaddr syntax in the config file.
    For example, to capture on two interfaces, rpcapd.ini would look like:
    
    ActiveClient = 10.10.6.45, 2003, ifaddr=10.1.0.0/16
    ActiveClient = 10.10.6.45, 2003, ifaddr=10.2.0.0/16
    NullAuthPermit = YES

--- winpcap/wpcap/libpcap/rpcapd/fileconf.c
+++ winpcap/wpcap/libpcap/rpcapd/fileconf.c
@@ -52,6 +52,12 @@ extern struct active_pars activelist[MAX_ACTIVE_LIST];		//!< Keeps the list of t
 extern int nullAuthAllowed;					//!< '1' if we permit NULL authentication, '0' otherwise
 extern char loadfile[MAX_LINE + 1];			//!< Name of the file from which we have to load the configuration
 
+struct ipaddr_spec {
+    int sa_family;
+    short prefix_len;
+    uint32_t addr[4];
+    uint32_t mask[4];
+};
 
 
 int strrem(char *string, char chr);
@@ -97,20 +103,179 @@ fileconf_sockaddr_to_text(struct sockaddr *addr, char *addrstr, int addrstrsize)
     }
 }
 
+static inline void
+ip6_make_mask(uint32_t *c, int n)
+{
+
+    c[0] = c[1] = c[2] = c[3] = 0;
+
+    while (n > 32) {
+        *c++ = 0xffffffffU;
+        n -= 32;
+    }
+    if (n != 0) {
+        *c = htonl(0xffffffffU << (32 - n));
+    }
+}
+
+static inline uint32_t
+ip4_maskcmp(uint32_t *a, uint32_t *b, uint32_t *mask)
+{
+    return ((a[0] & mask[0]) ^ (b[0] & mask[0]));
+}
+
+static inline uint32_t
+ip6_maskcmp(uint32_t *a, uint32_t *b, uint32_t *mask)
+{
+    return ((a[0] & mask[0]) ^ (b[0] & mask[0])) |
+           ((a[1] & mask[1]) ^ (b[1] & mask[1])) |
+           ((a[2] & mask[2]) ^ (b[2] & mask[2])) |
+           ((a[3] & mask[3]) ^ (b[3] & mask[3]));
+}
+
 static int
-fileconf_parse_ifname(struct active_pars *ap, char *ifname)
+ipaddr_spec_parse(char *addrstr, int addrstrlen,
+                  struct ipaddr_spec *spec)
+{
+    struct addrinfo hints;
+    struct addrinfo *res = NULL, *rp;
+    int rc = -1;
+    char ipstr[64];
+    char *p;
+    int prefix_len;
+
+    p = memchr(addrstr, '/', addrstrlen);
+    if (p != NULL) {
+        p[0] = '\0';
+        p++;
+    }
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_flags = AI_NUMERICHOST;
+    if (getaddrinfo(addrstr, NULL, &hints, &res) != 0) {
+        log_warn("ERROR: getaddrinfo '%s' failed", addrstr);
+        goto out;
+    }
+    if ((res == NULL) || (res->ai_addr == NULL) ||
+        ((res->ai_addr->sa_family != AF_INET) &&
+         (res->ai_addr->sa_family != AF_INET6))) {
+        log_warn("ERROR: bad addr '%s'", addrstr);
+        goto out;
+    }
+    spec->sa_family = res->ai_addr->sa_family;
+    if (spec->sa_family == AF_INET) {
+        memcpy(spec->addr, &((struct sockaddr_in *)res->ai_addr)->sin_addr, 4);
+    }
+    else {
+        memcpy(spec->addr, &((struct sockaddr_in6 *)res->ai_addr)->sin6_addr,
+               16);
+    }
+    if (p == NULL) {
+        prefix_len = (spec->sa_family == AF_INET) ? 32 : 128;
+    }
+    else {
+        prefix_len = atoi(p);
+        if ((prefix_len < 0) ||
+            ((spec->sa_family == AF_INET) && (prefix_len > 32)) ||
+            ((spec->sa_family == AF_INET6) && (prefix_len > 128))) {
+            log_warn("ERROR: bad prefix %d", prefix_len);
+            goto out;
+        }
+    }
+    spec->prefix_len = prefix_len;
+    ip6_make_mask(spec->mask, prefix_len);
+    rc = 0;
+
+ out:
+    if (res != NULL) {
+        freeaddrinfo(res);
+    }
+    return rc;
+}
+
+static int
+ipaddr_spec_cmp(struct ipaddr_spec *spec, struct sockaddr *sa)
+{
+    if (spec->sa_family != sa->sa_family) {
+        return -1;
+    }
+    if (spec->sa_family == AF_INET) {
+        struct sockaddr_in *in = (struct sockaddr_in *)sa;
+        return ip4_maskcmp(spec->addr, (uint32_t *)(&in->sin_addr),
+                           spec->mask);
+    }
+    else {
+        struct sockaddr_in6 *in6 = (struct sockaddr_in6 *)sa;
+        return ip6_maskcmp(spec->addr, (uint32_t *)(&in6->sin6_addr),
+                           spec->mask);
+    }
+    return 0;
+}
+
+static void
+ipaddr_spec_to_text(struct ipaddr_spec *spec, char *buf, int bufsize)
+{
+    union {
+        struct sockaddr sa;
+        struct sockaddr_in in;
+        struct sockaddr_in6 in6;
+    } u;
+    int rem_size;
+
+    buf[0] = '\0';
+    if ((bufsize == 0) || (spec->sa_family == 0)) {
+        return;
+    }
+    memset(&u.in6, 0, sizeof(struct sockaddr_in6));
+    u.sa.sa_family = spec->sa_family;
+    if (spec->sa_family == AF_INET) {
+        memcpy(&u.in.sin_addr, spec->addr, 4);
+    }
+    else {
+        memcpy(&u.in6.sin6_addr, spec->addr, 16);
+    }
+    fileconf_sockaddr_to_text(&u.sa, buf, bufsize);
+    rem_size = bufsize - strlen(buf);
+    buf = &buf[strlen(buf)];
+    snprintf(buf, rem_size, "/%d", spec->prefix_len);
+}
+
+static void
+fileconf_print_pcap_interfaces(pcap_if_t *alldevs)
+{
+    char addrstr[64];
+    pcap_if_t *d;
+    struct pcap_addr *addr;
+
+    log_warn("========== available pcap interfaces ==========");
+    for (d = alldevs; d != NULL; d = d->next) {
+        log_warn("   ifname=%s desc='%s'", d->name,
+                 (d->description != NULL) ? d->description : "");
+        for (addr = d->addresses; addr != NULL; addr = addr->next) {
+            fileconf_sockaddr_to_text(addr->addr,
+                                      addrstr, sizeof(addrstr));
+            log_warn("       addr %s", addrstr);
+        }
+    }
+    log_warn("===============================================");
+}
+
+static int
+fileconf_select_ifname(struct active_pars *ap, char *ifname,
+                       struct ipaddr_spec *ifaddr)
 {
     static int have_printed_ifs;
     char errbuf[PCAP_ERRBUF_SIZE + 1];
-    char addrstr[64];
     pcap_if_t *alldevs = NULL, *d;
     struct pcap_addr *addr;
     int rc = 1;
-    int print_ifs = 0;
 
     errbuf[0] = '\0';
 
-    if ((ifname == NULL) || (ifname[0] == '\0')) {
+    if ((ifname == NULL) && (ifaddr->sa_family == 0)) {
+        // no preselected interface
+        rc = 0;
         goto out;
     }
 
@@ -118,43 +283,46 @@ fileconf_parse_ifname(struct active_pars *ap, char *ifname)
         log_warn("ERROR: pcap_findalldevs: %s", errbuf);
         goto out;
     }
- print_interfaces:
-    if (print_ifs) {
-        log_warn("========== available pcap interfaces ==========");
-    }
     for (d = alldevs; d != NULL; d = d->next) {
-        if (print_ifs) {
-            log_warn("   ifname=%s desc='%s'", d->name,
-                     (d->description != NULL) ? d->description : "");
-            for (addr = d->addresses; addr != NULL; addr = addr->next) {
-                fileconf_sockaddr_to_text(addr->addr,
-                                          addrstr, sizeof(addrstr));
-                log_warn("       addr %s", addrstr);
+        int name_matches = 0;
+        int ip_matches = 0;
+
+        if ((ifname != NULL) && (strcmp(d->name, ifname) == 0)) {
+            name_matches = 1;
+        }
+        for (addr = d->addresses; addr != NULL; addr = addr->next) {
+            if ((addr->addr != NULL) && (ifaddr->sa_family != 0) &&
+                (ipaddr_spec_cmp(ifaddr, addr->addr) == 0)) {
+                ip_matches = 1;
             }
         }
-        if (strcmp(d->name, ifname) == 0) {
-            rc = 0;
+        if (((ifname == NULL) || name_matches) &&
+            ((ifaddr->sa_family == 0) || ip_matches)) {
             snprintf(ap->ifname, sizeof(ap->ifname), "%s", d->name);
+            rc = 0;
             break;
         }
-    }
-    if (print_ifs) {
-        log_warn("===============================================");
     }
     if (alldevs == NULL) {
         log_warn("ERROR: pcap_findalldevs returned no interfaces.");
         log_warn("ERROR: Please run rpcapd as administrator or root.");
     }
-    else if ((rc != 0) && !print_ifs) {
-        log_warn("WARNING: Could not find interface with name '%s'.", ifname);
+    else if (rc != 0) {
+        char ifaddr_buf[100];
+        ipaddr_spec_to_text(ifaddr, ifaddr_buf, sizeof(ifaddr_buf));
+        log_warn("WARNING: Could not find interface with"
+                 " ifname='%s' ifaddr='%s'.",
+                 (ifname != NULL) ? ifname : "",
+                 ifaddr_buf);
         if (!have_printed_ifs) {
             have_printed_ifs = 1;
-            print_ifs = 1;
-            goto print_interfaces;
+            fileconf_print_pcap_interfaces(alldevs);
         }
     }
  out:
-    pcap_freealldevs(alldevs);
+    if (alldevs != NULL) {
+        pcap_freealldevs(alldevs);
+    }
     return rc;
 }
 
@@ -162,11 +330,15 @@ static int
 fileconf_parse_activeclient_kv(struct active_pars *ap)
 {
     static int printed_space_warning;
+    struct ipaddr_spec ifaddr_spec;
     char *kv;
     char *key, *val;
     char *p;
+    char *ifname = NULL;
+    char *ifaddr = NULL;
     int rc = 0;
-    int rc2;
+
+    ifaddr_spec.sa_family = 0;
 
     while ((kv = strtok(NULL, RPCAP_HOSTLIST_SEP)) != NULL) {
         key = kv;
@@ -181,10 +353,15 @@ fileconf_parse_activeclient_kv(struct active_pars *ap)
             log_warn("WARNING: Make sure there are no spaces on"
                      " ActiveClient lines.");
         }
-        if (strcmp(key, "ifname") == 0) {
-            rc2 = fileconf_parse_ifname(ap, val);
-            if (rc2 != 0) {
-                rc = rc2;
+        if ((strcmp(key, "ifname") == 0) && (val != NULL) &&
+            (strlen(val) > 0)) {
+            ifname = val;
+        }
+        else if ((strcmp(key, "ifaddr") == 0) && (val != NULL) &&
+                 (strlen(val) > 0)) {
+            if (ipaddr_spec_parse(val, strlen(val), &ifaddr_spec) != 0) {
+                ifaddr_spec.sa_family = 0;
+                rc = -1;
             }
         }
         else {
@@ -193,6 +370,9 @@ fileconf_parse_activeclient_kv(struct active_pars *ap)
                rc = -1;
            }
         }
+    }
+    if (rc == 0) {
+        rc = fileconf_select_ifname(ap, ifname, &ifaddr_spec);
     }
     return rc;
 }
